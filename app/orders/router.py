7509e3eb2953a037d78b5e272d03b93cceb97b68
Adri Ceniza <adriacenizadev@gmail.com>
2026-07-23 21:41:36 +0100

    feat: add read_order endpoint and organize router tags

--- app/orders/router.py
+++ app/orders/router.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter, status
 from app.orders.schemas import OrderCreateRequest
-orders_router = APIRouter()
+orders_router = APIRouter(tags=['orders'])
 
-@orders_router.post('', tags=['orders'], status_code=status.HTTP_201_CREATED)
+@orders_router.post('', status_code=status.HTTP_201_CREATED)
 async def create_order(req: OrderCreateRequest):
     return {
         "order_id": "ord_123",
         "status": "created"
     }
+
+@orders_router.get('/{order_id}', status_code=status.HTTP_200_OK)
+async def read_order(order_id):
+    return
